ad85792faf9ece371a3a0639afae6adcdfeb5f18
maxmanyoung <gabby198young@gmail.com>
2025-10-28 13:35:28 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,30 @@
+enum RadioMessage {
+    message1 = 49434
+}
 input.onButtonPressed(Button.A, function () {
-    if (music.volume() > 100) {
-        music.setVolume(music.volume() + 10)
-        basic.showString("Hello!")
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    if (music.volume() < 200) {
+        music.setVolume(music.volume() + 20)
+        basic.showString("" + (music.volume() / 20))
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . . . . .
+            # # # # #
+            `)
+    } else {
+        basic.showString("Max")
     }
 })
 function random_music () {
-    music2 = [
-    0,
-    1,
-    2,
-    3,
-    4,
-    5,
-    6,
-    7,
-    8,
-    9
-    ]._pickRandom()
+    music2 = randint(0, 9)
     if (music2 == 0) {
         music._playDefaultBackground(music.builtInPlayableMelody(Melodies.Dadadadum), music.PlaybackMode.LoopingInBackground)
     } else if (music2 == 1) {
@@ -43,14 +51,35 @@ input.onButtonPressed(Button.AB, function () {
     random_music()
 })
 input.onButtonPressed(Button.B, function () {
-    music.setVolume(music.volume() - 10)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    if (music.volume() > 60) {
+        music.setVolume(music.volume() - 20)
+        basic.showString("" + (music.volume() / 20))
+    } else {
+        basic.showString("Min")
+    }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    music.stopAllSounds()
+    if (sound == 1) {
+        music.setBuiltInSpeakerEnabled(true)
+        sound = 0
+    } else {
+        music.setBuiltInSpeakerEnabled(false)
+        sound = 1
+    }
 })
-let speed = 0
 let distance = 0
+let speed = 0
+let sound = 0
 let music2 = 0
+music.setVolume(100)
+input.setSoundThreshold(SoundThreshold.Quiet, 50)
 basic.showString("Hello!")
 basic.showLeds(`
     # . # . #
@@ -59,20 +88,30 @@ basic.showLeds(`
     . . . . .
     # # # # #
     `)
-music.setVolume(50)
-loops.everyInterval(1000, function () {
-    distance = SuperBitV2_Digital.Ultrasonic(SuperBitV2_Digital.mwDigitalNum.P10P9)
-    while (distance < 10) {
-        music.play(music.createSoundExpression(WaveShape.Square, 1600, 1, 76, 0, 300, SoundExpressionEffect.None, InterpolationCurve.Curve), music.PlaybackMode.UntilDone)
-        SuperBitV2.Servo2(SuperBitV2.enServo.S1, 177)
-        basic.pause(1000)
-        SuperBitV2.Servo2(SuperBitV2.enServo.S1, 30)
-        break;
+basic.forever(function () {
+    if (SuperBitV2_Digital.PIR(SuperBitV2_Digital.mwDigitalNum.P4P6, SuperBitV2_Digital.enPIR.OPIR)) {
+        music.setBuiltInSpeakerEnabled(false)
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . . . . .
+            . # # # .
+            `)
+    } else {
+        music.setBuiltInSpeakerEnabled(true)
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . . . . .
+            # # # # #
+            `)
     }
 })
 basic.forever(function () {
     if (SuperBitV2_Analog.Potentiometer(SuperBitV2_Analog.mwAnalogNum.AP4P6) / 4 < 150) {
-        speed = SuperBitV2_Analog.Potentiometer(SuperBitV2_Analog.mwAnalogNum.AP4P6) / 4
+        speed = SuperBitV2_Analog.Potentiometer(SuperBitV2_Analog.mwAnalogNum.AP4P6) / 3.5
     } else {
         speed = 150
     }
@@ -103,5 +142,16 @@ basic.forever(function () {
         SuperBitV2.MotorRun(SuperBitV2.enMotors.M3, 0)
         SuperBitV2.RGB_Program().showColor(neopixel.colors(NeoPixelColors.White))
         SuperBitV2.RGB_Program().show()
+    }
+})
+basic.forever(function () {
+    let near_person = 0
+    distance = SuperBitV2_Digital.Ultrasonic(SuperBitV2_Digital.mwDigitalNum.P10P9)
+    while (near_person == 0 && distance < 10) {
+        music.play(music.createSoundExpression(WaveShape.Sawtooth, 1707, 347, 255, 0, 309, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve), music.PlaybackMode.InBackground)
+        SuperBitV2.Servo2(SuperBitV2.enServo.S1, 177)
+        basic.pause(1000)
+        SuperBitV2.Servo2(SuperBitV2.enServo.S1, 30)
+        break;
     }
 })
